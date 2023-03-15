948222644fe48476244ac0aa9a85c4fcd774ea09
Evan Cheng <evancheng2014@gmail.com>
2023-03-15 00:32:48 -0700

    add get video details

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const videos = require('./data/videos.json');
+const videoDetails = require('./data/video-details.json');
 
 app.get('/videos', (req, res) => {
 	res.send(videos);
+});
+
+app.get('/video/:videoId', (req, res) => {
+	const videoId = req.params.videoId;
+	const videoDetail = videoDetails.find(video => video.id === videoId);
+	res.send(videoDetail);
 });
 
 app.listen(8080, () => {
