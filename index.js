e8c3db1e439d60ed3d77eadd352567759076cce3
Evan Cheng <evancheng2014@gmail.com>
2023-03-15 00:08:08 -0700

    change image url in json

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const videos = require('./data/videos.json');
+
+app.get('/videos', (req, res) => {
+	res.send(videos);
+});
 
 app.listen(8080, () => {
 	console.log('Link Start!');
