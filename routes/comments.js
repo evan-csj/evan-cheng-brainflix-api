7cd1d6c0d1e51dfe8e8b6ce43f0c2a372965eeef
Evan Cheng <evancheng2014@gmail.com>
2023-03-17 01:37:19 -0700

    temp save

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,23 @@
 const { v4: uuid } = require('uuid');
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const options = 
 
 router.post('/:videoId/comments', (req, res) => {
+	const videoDetails = require('../data/video-details.json');
+	const videoId = req.params.videoId;
 	const newComment = req.body;
 	newComment.likes = 0;
 	newComment.timestamp = Date.now();
 	newComment.id = uuid();
+
+	const videoDetailsJSON = videoDetails.find(
+		element => element.id === videoId
+	);
+	videoDetailsJSON.comments.push(newComment);
+	console.log(videoDetails);
+
 	res.send(newComment);
 });
 
