4004f2db920053849eb048c8eeba5a3bef183416
Evan Cheng <evancheng2014@gmail.com>
2023-03-16 17:30:26 -0700

    add post comment api

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const PORT = 8080;
 const videosRoutes = require('./routes/videos');
 const videoDetailsRoutes = require('./routes/video-details');
+const commentsRoute = require('./routes/comments');
 
-app.use('/videos', [videosRoutes, videoDetailsRoutes]);
+app.use(express.json());
+app.use('/videos', [videosRoutes, videoDetailsRoutes, commentsRoute]);
 
 app.listen(PORT, () => {
 	console.log('Link Start!');
--- routes/comments.js
+++ routes/comments.js
@@ -1,2 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+
+router.post('/:videoId/comments', (req, res) => {
+	console.log(req.body);
+	res.send(req.params.videoId);
+});
+
+module.exports = router;
