f14b687f588a67e9ac8ad3acdf5fec31e632be17
Evan Cheng <evancheng2014@gmail.com>
2023-03-16 22:02:02 -0700

    add cors and change url for images

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 8080;
+
 const videosRoutes = require('./routes/videos');
 const videoDetailsRoutes = require('./routes/video-details');
 const commentsRoute = require('./routes/comments');
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('./public/images'));
 app.use('/videos', [videosRoutes, videoDetailsRoutes, commentsRoute]);
 
 app.listen(PORT, () => {
