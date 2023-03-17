81bea52f7b460b02c6afde57af752ecf1636e928
Evan Cheng <evancheng2014@gmail.com>
2023-03-17 00:20:23 -0700

    add delete comment feature and uuid for each comment

--- routes/comments.js
+++ routes/comments.js
@@ -1,3 +1,4 @@
+const { v4: uuid } = require('uuid');
 const express = require('express');
 const router = express.Router();
 
@@ -5,8 +6,13 @@ router.post('/:videoId/comments', (req, res) => {
 	const newComment = req.body;
 	newComment.likes = 0;
 	newComment.timestamp = Date.now();
-	console.log(newComment)
-	res.send(req.body);
+	newComment.id = uuid();
+	res.send(newComment);
+});
+
+router.delete('/:videoId/comments/:commentId', (req, res) => {
+	const deleteComment = req.body;
+	res.send(deleteComment);
 });
 
 module.exports = router;
