4dad68348f553f4af4d54fe0b2288555cf200457
Evan Cheng <evancheng2014@gmail.com>
2023-03-18 23:41:08 -0700

    add new videos

--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { v4: uuid } = require('uuid');
 const router = express.Router();
+const lib = require('../lib');
 const videos = require('../data/videos.json');
 const videosFileName = 'videos.json';
 const videoDetailsFileName = 'video-details.json';
@@ -10,12 +11,30 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req, res) => {
-	// const newVideo = req.body;
-	// const newVideoDetails = req.body;
-	// const newId = uuid();
-	// newVideo.id = newId;
-	// newVideoDetails.id = newId;
-	// newVideoDetail
+	const newVideo = req.body;
+	const newId = uuid();
+	newVideo.id = newId;
+	const newVideoDetails = {
+		id: newId,
+		title: req.body.title,
+		channel: req.body.channel,
+		image: req.body.image,
+		description: req.body.description,
+		views: 0,
+		likes: 0,
+		duraion: '4:58',
+		timestamp: Date.now(),
+		comments: [],
+	};
+
+	const videos = lib.readJSON(videosFileName);
+	const videoDetails = lib.readJSON(videoDetailsFileName);
+	videos.push(newVideo);
+	videoDetails.push(newVideoDetails);
+	lib.writeJSON(videosFileName, videos);
+	lib.writeJSON(videoDetailsFileName, videoDetails);
+
+	res.send();
 });
 
 module.exports = router;
